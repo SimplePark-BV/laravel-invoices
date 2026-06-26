0abec3c1bf4d9a30a30745286bd97364125354d5
Jasper Demmers <jasper@simplepark.nl>
2026-06-26 10:24:05 +0200

    feat: Add credit transfer request (#32)
    * feat: Add translations
    
    * feat: Adjust message shown
    
    * fix: Adjust translations

--- resources/lang/en/invoice.php
+++ resources/lang/en/invoice.php
@@ -17,5 +17,6 @@ return [
     'bank' => 'Bank:',
     'unknown' => 'Unknown',
     'payment_request' => 'We kindly request you to pay the above amount of :amount by :date to our bank account. For questions, please contact us by email.',
+    'credit_transfer_request' => 'We will transfer the above amount of :amount to your bank account by :date. For questions, please contact us by email.',
     'concept_message' => 'This is a <strong>DRAFT</strong>. This means this invoice is not yet official and no payment is required. You will receive the official invoice at a later date.',
 ];
